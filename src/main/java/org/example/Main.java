70047d43e191095241b83038eb61b162c64a164b
FaridaFatali <faridafatali.91@gmail.com>
2023-09-06 23:33:48 +0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Practice: <a href="https://www.youtube.com/watch?v=GMdgjaDdOjI">...</a>
+ * @author Farida Fatali
+ * Black Jack Game in Java
+ */
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BlackJack blackJack = new BlackJack();
+    }
+}
